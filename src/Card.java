2657d7a9cd7bd772a7fdc1cfc6a071179f709075
Håvard Anda Estensen <haavard.ae@gmail.com>
2017-03-21 12:49:35 +0100

    Add getters

--- src/Card.java
+++ src/Card.java
@@ -30,4 +30,12 @@ public class Card {
         this.rank = rank;
         this.suit = suit;
     }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
 }
